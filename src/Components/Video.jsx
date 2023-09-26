0eefb31cdfddf70708cf6ac885a345b7136c3a86
dylant313 <dylant313@yahoo.com>
2023-09-26 18:10:22 -0400

    Added css file

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import "./Video.css"
 
 const Video = ({ link }) => {
   const [ytLink, setytLink] = useState("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
@@ -9,15 +10,17 @@ const Video = ({ link }) => {
   }
 
   return (
-    <iframe 
-      width="560" 
-      height="315" 
-      src={ytLink.replace('watch?v=', 'embed/')} 
-      title="YouTube video player" 
-      frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-      allowFullScreen>
-    </iframe>
+    <div class='videoContainer'>
+      <iframe 
+        width="560" 
+        height="315" 
+        src={ytLink.replace('watch?v=', 'embed/')} 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowFullScreen>
+      </iframe>
+    </div>
   )
 }
 
