09a4316119f74e08068b2f90e7471f640c99caaf
dylant313 <dylant313@yahoo.com>
2023-09-26 18:32:19 -0400

    Added placeholder pages to router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import ScrollQueue from './ScrollQueue.jsx'
-import Video from './Components/Video/Video'
+
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Dashboard from "./Pages/Dashboard/Dashboard"
+import Login from "./Pages/Login/Login";
 
 function App() {
   
-  
+  /*
+    To add a new page, make a new element in the Pages folder and add a
+    new Route below linking that element to the corresponding URL path.
+  */
   return (
-    <div className="App">
-      <header className="App-header">
-        <Video/>
-      </header>
-      <ScrollQueue/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </Router>
   );
 }
 
