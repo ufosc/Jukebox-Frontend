fa5f926e580c6c8f594f0658f08094cb885c6664
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-09-28 19:07:59 -0400

    Add basic elements and styles to Hero component

--- src/Pages/Landing/components/Hero.js
+++ src/Pages/Landing/components/Hero.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import classes from './Hero.module.css';
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className={classes.hero}>
+      <div className={classes.hero__left}>
+        <h1>
+          Welcome to the&nbsp;
+          <span className={classes.title__highlight}>Jukebox</span>
+          &nbsp;Project
+        </h1>
+        <p className={classes.title__description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sunt reprehenderit molestias laudantium in impedit blanditiis enim et id distinctio. Earum ad nemo non facere ipsa deserunt beatae assumenda quibusdam!
+        </p>
+        <div className={classes.github__container}>
+          <div className={classes.github__line}></div>
+          <button type="button">Visit GitHub</button>
+        </div>
+      </div>
+      <div className={classes.hero__right}>
+
+      </div>
+
+    </section>
   )
 }
