380cfc24331bc619469c274c43ffc4df10067ac4
Jonathan Hooth <poprtalmaster9999@gmail.com>
2023-09-28 18:02:48 -0400

    Board 1 without router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
+import './index.css';
 import './App.css';
 
+//Temporary Clock
+import Clock from './components/Clock'
+import ScrollQueue from './components/ScrollQueue'
+
+
+
+
 function App() {
+  var url = "bMknfKXIFA8"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1 className = "center">
+        Jukebox
+        </h1>
       </header>
+      <body>
+        <div>
+          <Clock></Clock>
+          
+          {/*<SongInput></SongInput>*/}
+          <ScrollQueue></ScrollQueue>
+        </div>
+        
+      </body>
     </div>
-  );
+  )
 }
 
 export default App;
+
