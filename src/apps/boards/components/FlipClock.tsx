5fff5f1e31fd06500d1262f35fd883dc9a651694
Isaac Hunter <ikehunter5@gmail.com>
2024-09-11 13:49:31 -0400

    style flipclock, start flip animations

--- src/apps/boards/components/FlipClock.tsx
+++ src/apps/boards/components/FlipClock.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useTime } from 'src/hooks'
+import './FlipClock.scss'
 
+// ANIMATION IS WORK-IN-PROGRESS
 export const FlipClock = () => {
   const { date, hours: currentHours, minutes: currentMinutes } = useTime()
   const [hours, setHours] = useState(currentHours)
@@ -12,13 +14,21 @@ export const FlipClock = () => {
     <div className="flip-clock">
       <time dateTime={date.toLocaleDateString()}>
         <span className="flip-clock__hour">
-          <span className="flip-clock__digit">{hours[0]}</span>
-          <span className="flip-clock__digit">{hours[1]}</span>
+          <span className="flip-clock__digit" data-value={hours[0]}>
+            {hours[0]}
+          </span>
+          <span className="flip-clock__digit" data-value={hours[1]}>
+            {hours[1]}
+          </span>
         </span>
         <span className="flip-clock__colon">:</span>
         <span className="flip-clock__min">
-          <span className="flip-clock__digit">{minutes[0]}</span>
-          <span className="flip-clock__digit">{minutes[1]}</span>
+          <span className="flip-clock__digit" data-value={minutes[0]}>
+            {minutes[0]}
+          </span>
+          <span className="flip-clock__digit" data-value={minutes[1]}>
+            {minutes[1]}
+          </span>
         </span>
       </time>
     </div>
