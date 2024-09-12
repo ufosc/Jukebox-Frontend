510e1ac901913ed45c38607b9f16b23dd776901c
Isaac Hunter <ikehunter5@gmail.com>
2024-09-12 11:18:32 -0400

    return more info from time hook, additional styles to flip clock

--- src/apps/boards/components/FlipClock.tsx
+++ src/apps/boards/components/FlipClock.tsx
@@ -1,5 +1,10 @@
+/**
+ * Inspiration:
+ * https://codepen.io/shshaw/pen/vKzoLL?editors=0110
+ */
 import { useEffect, useState } from 'react'
 import { useTime } from 'src/hooks'
+import { sleep } from 'src/utils'
 import './FlipClock.scss'
 
 // ANIMATION IS WORK-IN-PROGRESS
@@ -7,11 +12,27 @@ export const FlipClock = () => {
   const { date, hours: currentHours, minutes: currentMinutes } = useTime()
   const [hours, setHours] = useState(currentHours)
   const [minutes, setMinutes] = useState(currentMinutes)
+  const [flip, setFlip] = useState(false)
 
-  useEffect(() => {}, [date])
+  useEffect(() => {
+    // setFlip(true)
+    setMinutes(currentMinutes)
+    setHours(currentHours)
+
+    // sleep(1000).then(() => setFlip(false))
+  }, [date])
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setFlip(true)
+  //     sleep(1000).then(() => setFlip(false))
+  //   }, 5000)
+
+  //   return () => clearInterval(interval)
+  // }, [])
 
   return (
-    <div className="flip-clock">
+    <div className={`flip-clock${(flip && ' flip') || ''}`}>
       <time dateTime={date.toLocaleDateString()}>
         <span className="flip-clock__hour">
           <span className="flip-clock__digit" data-value={hours[0]}>
