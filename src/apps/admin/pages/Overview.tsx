24905fea191c3727d516bd9ae95171ab6712032a
Isaac Hunter <ikehunter5@gmail.com>
2024-09-21 01:34:36 -0400

    fix vscode settings for windows

--- src/apps/admin/pages/Overview.tsx
+++ src/apps/admin/pages/Overview.tsx
@@ -1,78 +1,65 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState } from 'react'
 
-import { AudioPlayer } from "src/components"
+import { AudioPlayer } from 'src/components'
 
-
-
-import "./Overview.scss"
-
-
+import './Overview.scss'
 
 import { SpotifyPlayerContext } from 'src/context'
-import { mockTrack } from "src/mock"
+import { mockTrack } from 'src/mock'
 import Disk from './Disk.svg?react'
 
-import { Track } from "./Track"
+import { Track } from './Track'
 
 export const Overview = () => {
-
-  const [song, setSong] = useState('');
-  const [author, setAuthor] = useState('');
+  const [song, setSong] = useState('')
+  const [author, setAuthor] = useState('')
   const { currentTrack } = useContext(SpotifyPlayerContext)
-  
 
-  useEffect(()=>
-  {
-    console.log("Hello")
+  useEffect(() => {
+    console.log('Hello')
     console.log('Current track:', currentTrack)
-    console.log( )
-    if(currentTrack == undefined)
-    {
-      setSong("No song Playing")
-      setAuthor("No Author")
-    }else{
-      setSong(currentTrack.name);
-      setAuthor(currentTrack.artists.map((artist: { name: any }) => artist.name).join(', '))
+    console.log()
+    if (currentTrack == undefined) {
+      setSong('No song Playing')
+      setAuthor('No Author')
+    } else {
+      setSong(currentTrack.name)
+      setAuthor(
+        currentTrack.artists
+          .map((artist: { name: any }) => artist.name)
+          .join(', '),
+      )
     }
+  }, [currentTrack])
 
-  },[currentTrack])
-
-  const track = mockTrack;
+  const track = mockTrack
   return (
     <>
-
       <div className="grid">
         <div className="col-5 card">
           <div className="song-desc">
-            <div className="song-title">
-              {song}
-              
-            </div>
-            <div className="song-author">
-              {author}
-            </div>
+            <div className="song-title">{song}</div>
+            <div className="song-author">{author}</div>
           </div>
           <AudioPlayer />
         </div>
 
         <div className="col-7">
           <div className="disk">
-            
-            <img className="curr-song"  src={currentTrack?.album?.images[0].url} alt={track.name}/>
+            <img
+              className="curr-song"
+              src={currentTrack?.album?.images[0].url}
+              alt={track.name}
+            />
             <Disk />
-            
-            
           </div>
-
         </div>
-
       </div>
 
       <div className="grid">
-      <div className="col-12">
+        <div className="col-12">
           <div className="song-queue scrollbar">
             <ol className="board__queue__list track-list scrollbar">
-              
               <Track track={track} />
               <Track track={track} />
               <Track track={track} />
@@ -80,10 +67,8 @@ export const Overview = () => {
               <Track track={track} />
             </ol>
           </div>
-
         </div>
       </div>
-
     </>
   )
 }
