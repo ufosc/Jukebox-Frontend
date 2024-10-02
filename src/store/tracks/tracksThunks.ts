c43d3c3bc6197d709227ff694679dac75f40aad8
Jonathan Hooth <portalmaster9999@gmail.com>
2024-10-02 18:37:17 -0400

    Now displays placeholder image when not connected

---
+++ src/store/tracks/tracksThunks.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { Network } from 'src/network'
+
+const network = Network.getInstance()
+
+export const thunkFetchCurrentTrack = createAsyncThunk(
+    '',
+    async(
+
+        
+    )
+
+
+)
