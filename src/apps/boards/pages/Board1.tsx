c6f617c3fa389c780a068dd91aa85c651f8f3ac7
Isaac Hunter <ikehunter5@gmail.com>
2024-11-07 11:24:25 -0500

    remove player from board1, add ts build gh action

--- src/apps/boards/pages/Board1.tsx
+++ src/apps/boards/pages/Board1.tsx
@@ -1,20 +1,14 @@
-import { useContext, useEffect } from 'react'
-import { SpotifyPlayerContext } from 'src/context'
-import { mockTrack } from 'src/mock'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectCurrentTrack, selectNextTracks } from 'src/store/track'
 import { Clock, TracksPanel } from '../components'
 import { DisplayPanel } from '../components/DisplayPanel'
 import './Board1.scss'
 
 export const Board1 = () => {
-  const { currentTrack } = useContext(SpotifyPlayerContext)
-
-  const track = mockTrack
-  const nextTracks = [
-    track,
-    { ...track, id: track.id + '1' },
-    { ...track, id: track.id + '2' },
-    { ...track, id: track.id + '3' },
-  ]
+  // const { currentTrack } = useContext(SpotifyPlayerContext)
+  const currentTrack = useSelector(selectCurrentTrack)
+  const nextTracks = useSelector(selectNextTracks)
 
   useEffect(() => {
     console.log('Current track:', currentTrack)
