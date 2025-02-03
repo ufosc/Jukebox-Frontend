e7e77fa1b77117ed0a61a2898344c418d7c2ba41
Ben <bpratt@ihmc.org>
2025-02-03 13:38:53 -0500

    Adds the topbar to members page

--- src/apps/members/pages/Home.tsx
+++ src/apps/members/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.scss'
 import { Sidebar } from '../components/Sidebar'
+import { Topbar } from '../components/Topbar'
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
+      <Topbar />
     </div>
   )
 }
