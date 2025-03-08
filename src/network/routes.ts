d51347e45ab9988d3352c26c82cf92aec9b057de
Isaac Hunter <ikehunter5@gmail.com>
2025-03-07 22:34:43 -0500

    fix network auth

--- src/network/routes.ts
+++ src/network/routes.ts
@@ -1,29 +1,30 @@
-import { NETWORK_URL } from 'src/config'
+import { CLUBS_URL, JUKEBOX_URL } from 'src/config'
 
-const base = NETWORK_URL + '/api/v1'
+const jukeboxApi = JUKEBOX_URL + '/api/v1'
+const clubsApi = CLUBS_URL + '/api/v1'
 
 export const NetworkRoutes = {
   user: {
-    token: `${base}/user/token/`,
-    login: `${base}/user/login/`,
-    details: `${base}/user/me/`,
+    token: `${clubsApi}/user/token/`,
+    login: `${clubsApi}/user/login/`,
+    details: `${clubsApi}/user/me/`,
   },
   club: {
-    list: `${base}/club/clubs/`,
-    info: (id: number) => `${base}/club/clubs/${id}/`,
+    list: `${clubsApi}/club/clubs/`,
+    info: (id: number) => `${clubsApi}/club/clubs/${id}/`,
   },
   jukebox: {
-    list: `${base}/jukebox/jukeboxes/`,
+    list: `${jukeboxApi}/jukebox/jukeboxes/`,
     activeLink: (jukeboxId: number) =>
-      `${base}/jukebox/${jukeboxId}/active-link/`,
+      `${jukeboxApi}/jukebox/${jukeboxId}/active-link/`,
     refreshSpotifyToken: (jukeboxId: number) =>
-      `${base}/jukebox/${jukeboxId}/active-link/`,
+      `${jukeboxApi}/jukebox/${jukeboxId}/active-link/`,
     // `${base}/jukebox/${jukeboxId}/active-link/?force-refresh=true`,
     connectDevice: (jukeboxId: number) =>
-      `${base}/jukebox/${jukeboxId}/connect/`,
+      `${jukeboxApi}/jukebox/${jukeboxId}/connect/`,
     playerState: (jukeboxId: number) =>
-      `${base}/jukebox/${jukeboxId}/player-state/`,
+      `${jukeboxApi}/jukebox/${jukeboxId}/player-state/`,
     nextTracks: (jukeboxId: number) =>
-      `${base}/jukebox/${jukeboxId}/tracks-queue/`,
+      `${jukeboxApi}/jukebox/${jukeboxId}/tracks-queue/`,
   },
 }
