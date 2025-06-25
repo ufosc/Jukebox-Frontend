b6e1ce1e8b1076b99983f977fea456ed469be179
Jonathan Hooth <portalmaster9999@gmail.com>
2025-06-25 16:53:03 -0400

    Rough draft of Search modal

--- src/apps/admin/components/modals/SearchModal.tsx
+++ src/apps/admin/components/modals/SearchModal.tsx
@@ -2,25 +2,43 @@ interface SearchModalProps {
   tracks: ITrackDetails[]
 }
 
+import { ViewArrow } from 'src/assets/Icons'
 import './Modal.scss'
 
 export const SearchModal = ({ tracks }: SearchModalProps) => {
   return (
     <>
       <div className="modal modal__search">
-        <div className='modal__search__track-list'>
+        <div className="modal__search__track-list">
           {tracks.splice(0, 4).map((track, key) => (
             <div className="modal__search__track-list__item">
-              <div className='modal__search__title'>
-                {track.name}
-              </div>
+              <div className="modal__search__title">{track.name}</div>
 
-              <div>
-                {track.album.name}
+              <div className="modal__search__information">
+                <div className="modal__search__information__item">
+                  {track.artists[0].name}
+                  {track.artists.length > 1 ? <>...</> : <></>}
+                </div>
+
+                <div className="modal__search__information__album">
+                  {track.album.name}
+                </div>
+
+                <div className="modal__search__information__detail">
+                  {track.album.release_date.split('-')[0]}
+                </div>
+
+                <div className="modal__search__information_detail">
+                  {track.explicit ? <>Explicit</> : <>Clean</>}
+                </div>
               </div>
             </div>
           ))}
         </div>
+        {tracks.length > 0 ?(<div className="modal__search__more">
+            View All
+            <ViewArrow color={'color-primary'} />
+          </div>): (<></>)}
       </div>
     </>
   )
